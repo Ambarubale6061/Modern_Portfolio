32aa4d0ce3d4a2beef6f3458ae414cbd8ee5067b
Ambar Ubale <ambarubale3333@gmail.com>
2026-07-17 17:28:06 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      { source: "/home", destination: "/" },
+      { source: "/about", destination: "/" },
+      { source: "/skills", destination: "/" },
+      { source: "/work", destination: "/" },
+      { source: "/experience", destination: "/" },
+      { source: "/contact", destination: "/" },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
